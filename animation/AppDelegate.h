d4db4451514158feb1f428a50682438868abcdfc
yao.xue <yao.xue@qunar.com>
2015-04-28 16:31:29 +0800

    Initial Commit

---
+++ animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  animation
+//
+//  Created by xueyao on 15/4/28.
+//  Copyright (c) 2015年 xueyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  animation
+//
+//  Created by xueyao on 15/4/28.
+//  Copyright (c) 2015年 xueyao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
